7246e88f0c2a2fd15ade320ffb1af6ea72708b46
Seph Gentle <me@josephg.com>
2025-12-12 19:07:44 +1100

    Removed multiboot child crate

--- multiboot/build.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    println!("cargo:rerun-if-changed=linker32.lds");
-    println!("cargo:rustc-link-arg=-Tlinker32.lds");
-    println!("cargo:rustc-link-arg=-no-pie");
-    println!("cargo:rustc-link-arg=-static"); // optional, see below
-}
