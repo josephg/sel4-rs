4ed9c280c1c6ab8873d73a9eee1e655c94321ff0
Seph Gentle <me@josephg.com>
2025-12-15 17:19:38 +1100

    Tweaked run script to bail on compile error. Also added -initrd with sample root image

--- run_debug.sh
+++ run_debug.sh
@@ -1,4 +1,8 @@
-cargo build -p kernel && objcopy -O elf32-i386 target/x86_64-unknown-none/debug/kernel kernel.elf
+#!/usr/bin/env bash
+set -e
+
+cargo build -p kernel
+objcopy -O elf32-i386 target/x86_64-unknown-none/debug/kernel kernel.elf
 
 echo 'Ctrl+A, X to terminate QEMU'
-qemu-system-x86_64 -enable-kvm -cpu host -serial mon:stdio -m size=512M -kernel kernel.elf  -no-reboot -d cpu_reset -d int
+qemu-system-x86_64 -enable-kvm -cpu host -serial mon:stdio -m size=512M -kernel kernel.elf -initrd hello-image-x86_64-pc99 -no-reboot -d cpu_reset -d int
